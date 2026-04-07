29072e4b39bf4f1c698c083ec5bf73a34ec7b804
mikkel <m.tammenga@hotmail.com>
2026-04-07 19:17:48 +0200

    added routes

--- iot-visuals/server_code/server.py
+++ iot-visuals/server_code/server.py
@@ -39,6 +39,14 @@ def temperature():
 
     return jsonify(response_data)
 
+@app.route("/")
+def index():
+    return render_template("index.html") # Serveer de HTML pagina
+
+@app.route("/get_status")
+def get_status():
+    return jsonify(last_status) # Endpoint voor de HTML pagina
+
 if __name__ == "__main__":
     # Host 0.0.0.0 zorgt dat de Pico verbinding kan maken via je netwerk
     app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
